103dac58c711a0567bfd346cc5ce44f244eab8e1
Felipe <nascimentofelipe565@gmail.com>
2021-04-28 16:28:03 -0300

    first request done

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+let requestXML = new XMLHttpRequest();
+var endpoint = "http://ip-api.com/json/?fields=status,message,countryCode";
+
+requestXML.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    let response = JSON.parse(this.responseText);
+
+    if (response.status !== "success") {
+      console.log("query failed: " + response.message);
+      return;
+    }
+
+    if (response.countryCode == "US") {
+      window.location.replace("https://google.com/");
+    }
+    if (response.countryCode == "CA") {
+      window.location.replace("https://google.ca/");
+    }
+    if (response.countryCode == "BR") {
+      window.location.replace("https://facebook.com/");
+    }
+  }
+};
+
 document
   .querySelector("#button-submit")
   .addEventListener("click", function (e) {
@@ -9,6 +33,9 @@ document
     if (name.value == "ademiro") {
       if (password.value == "hu3hu3") {
         document.querySelector("#welcome-message").className = "texto-welcome";
+
+        requestXML.open("GET", endpoint, true);
+        requestXML.send();
 
         return alert("Top, entrou");
       } else {
