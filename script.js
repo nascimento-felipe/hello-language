256914690b3c8a749cd1db859c93af6cf37f5ccc
Felipe <nascimentofelipe565@gmail.com>
2021-04-28 22:53:13 -0300

    project finished

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let requestXML = new XMLHttpRequest();
-var endpoint = "http://ip-api.com/json/?fields=status,message,countryCode";
+var endpoint = "http://ip-api.com/json/?fields=status,message,query";
+
+let username = "";
 
 requestXML.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
@@ -10,15 +12,17 @@ requestXML.onreadystatechange = function () {
       return;
     }
 
-    if (response.countryCode == "US") {
-      window.location.replace("https://google.com/");
-    }
-    if (response.countryCode == "CA") {
-      window.location.replace("https://google.ca/");
-    }
-    if (response.countryCode == "BR") {
-      window.location.replace("https://facebook.com/");
-    }
+    console.log(response.query);
+    fetch(`https://fourtonfish.com/hellosalut/?ip=${response.query}`)
+      .then(function (responseHello) {
+        return responseHello.json();
+      })
+      .then(function (json) {
+        console.log(json);
+        document.querySelector("#hello-p").innerHTML = `${
+          json.hello + " " + username
+        }, you have successfully logged in!`;
+      });
   }
 };
 
@@ -30,22 +34,31 @@ document
     const name = document.querySelector("#name");
     const password = document.querySelector("#password");
 
-    if (name.value == "ademiro") {
-      if (password.value == "hu3hu3") {
-        document.querySelector("#welcome-message").className = "texto-welcome";
-
-        requestXML.open("GET", endpoint, true);
-        requestXML.send();
-
-        return alert("Top, entrou");
+    if (name.value == "" || password.value == "") {
+      if (name.value == "" && password.value != "") {
+        password.style.borderColor = "";
+        name.style.borderColor = "red";
+        return alert("Hey, name field is empty...");
+      } else if (name.value != "" && password.value == "") {
+        name.style.borderColor = "";
+        password.style.borderColor = "red";
+        return alert("Hey, password field is empty...");
       } else {
-        password.value = "";
-        return alert("The password is wrong!");
+        name.style.borderColor = "red";
+        password.style.borderColor = "red";
+        return alert("Hey, both fields are empty!");
       }
     } else {
-      name.value = "";
-      password.value = "";
-      return alert("The name is wrong!");
+      name.style.borderColor = "";
+      name.style.borderColor = "";
+      document.querySelector("#welcome-message").className = "texto-welcome";
+
+      username = name.value;
+
+      requestXML.open("GET", endpoint, true);
+      requestXML.send();
+
+      return;
     }
   });
 
@@ -54,9 +67,10 @@ document
   .addEventListener("click", function (e) {
     e.preventDefault();
 
+    document.querySelector(
+      "#hello-p"
+    ).innerHTML = `Have a great day, ${username}!`;
+
     document.querySelector("#name").value = "";
     document.querySelector("#password").value = "";
-
-    document.querySelector("#welcome-message").className =
-      "texto-welcome-hidden";
   });
